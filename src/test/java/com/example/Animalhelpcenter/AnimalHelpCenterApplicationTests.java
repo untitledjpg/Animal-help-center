2768faa8a32327f3a6605cffab512e8d3e6fa308
Olga <egliteolga@gmail.com>
2021-03-22 20:51:05 +0200

    Initial commit

---
+++ src/test/java/com/example/Animalhelpcenter/AnimalHelpCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Animalhelpcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalHelpCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
