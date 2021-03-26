e0038d752d531b3f91a684b25503e24bcc4ee492
Olga <egliteolga@gmail.com>
2021-03-27 01:05:34 +0200

    commiting adoption form, addAdoptApplication in DB

--- src/main/java/com/example/Animalhelpcenter/data/DatabaseManager.java
+++ src/main/java/com/example/Animalhelpcenter/data/DatabaseManager.java
@@ -3,6 +3,7 @@ package com.example.Animalhelpcenter.data;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +29,41 @@ public class DatabaseManager {
         }
 
         return cats;
+    }
+
+
+    public Integer addAdoptApplication(AdoptionApplication app) { // stored procedure
+        Connection con = null;
+        try {
+            con = getConnection();
+
+            var stmt = con.prepareCall("{CALL addAdoptApplication(?,?,?,?,?,?,?,?,?)}");
+
+            stmt.setInt("id", app.getId());
+            stmt.setString("name", app.getName());
+            stmt.setString("surname", app.getSurname());
+            stmt.setInt("phone_number", app.getPhoneNumber());
+            stmt.setString("email", app.getEmail());
+            stmt.setInt("catId", app.getCatId());
+            stmt.setString("other_pets", app.getOtherPets());
+            stmt.setString("children", app.getChildren());
+
+            stmt.registerOutParameter("new_id", Types.INTEGER); // out
+
+            stmt.executeUpdate(); // should return id
+
+            Integer id = stmt.getInt("new_id");
+
+            app.setId(id);
+
+            con.close();
+
+            return id;
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
     }
 
 
--- src/main/java/com/example/Animalhelpcenter/mvc/MainController.java
+++ src/main/java/com/example/Animalhelpcenter/mvc/MainController.java
@@ -1,24 +1,52 @@
 package com.example.Animalhelpcenter.mvc;
 
+import com.example.Animalhelpcenter.data.AdoptionApplication;
 import com.example.Animalhelpcenter.data.DatabaseManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
 
 @Controller
 public class MainController {
 
+    DatabaseManager dm = new DatabaseManager();
+
     @GetMapping("")
     public String getIndexPage(Model model){
 
-        var dm = new DatabaseManager();
         var cats = dm.getCats();
         model.addAttribute("cats",cats);             //key, object
         return "index";
     }
 
     @GetMapping("/adopt")
-    public String getAdoptPage(Model model){
+    public String addAdoptApplicationToDB(Model model){
+
+        var cats = dm.getCats();
+        model.addAttribute("cats", cats);
+
         return "adopt";
     }
+
+    @PostMapping("/adopt")
+    public ModelAndView addCity(@ModelAttribute("addAdoptionApplication") AdoptionApplication dto){
+
+        var app = new AdoptionApplication(0, dto.getName(), dto.getSurname(), dto.getPhoneNumber(),
+                dto.getEmail(), dto.getCatId(), dto.getOtherPets(), dto.getChildren());
+        dm.addAdoptApplication(app );
+
+        return new ModelAndView("redirect:/app/sent"); // redirect
+    }
+
+
+    @GetMapping("/app/sent")
+    public String getApplicationSent(Model model){
+        return "app_sent";
+    }
 }
+
